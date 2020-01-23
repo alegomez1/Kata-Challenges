5fb55394cd81559133070d82533e5c6ad6c2c953
agomez <alegomez1@me.com>
2020-01-23 09:44:46 -0500

    Finished generate_integer_range.rb

---
+++ generate_integer_range.rb
@@ -0,0 +1,12 @@
+#https://www.codewars.com/kata/55eca815d0d20962e1000106/train/ruby
+def generate_range(min, max, step)
+  range = []
+  start = min
+  (min..max).each do |i|
+    if start <= max
+      range.push(start)
+      start = start += step
+    end
+  end
+  return range
+end
